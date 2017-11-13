b8ba9e6c12bc1e69e74b5b1690346b7326075ab4
Zinthrow <33006657+Zinthrow@users.noreply.github.com>
2017-11-13 17:37:12 -0600

    Update Anchor_alignment.py

--- Anchor_alignment.py
+++ Anchor_alignment.py
@@ -41,41 +41,56 @@ def initialize():
             elif indy is 0 and indx > 0:
                 Ix[indy][indx] = Ixs(indx,indy)
             elif indx is 0 and indy > 0:
-                Iy[indy][indx] = Iys(indx, indy)
-                
+                Iy[indy][indx] = Iys(indx,indy)
+               
+            if indx in anchor_pairs:
+                if anchor_pairs[indx] == indy:
+                    D[indy][indx] = D[indy][indx] + p1
+                    Ix[indy][indx] = Ix[indy][indx] + p1
+                    Iy[indy][indx] = Iy[indy][indx] + p1
+                    print ("tripped")
 def box_max(i,j):
     return  max(D[j][i],
-            Ix[j][i],
-            Iy[j][i])
+                Ix[j][i],
+                Iy[j][i])
                
-def recurse():
-    m_align = m[m_len-1]
-    n_align = n[n_len-1]
+def align():
+    m_align = ""
+    n_align = ""
     indx = m_len-1
     indy = n_len-1
+    #current = 0
+    if box_max(indx,indy) == D[indy][indx]:
+        m_align = m[indx] + m_align
+        n_align = n[indy] + n_align
+    elif box_max(indx,indy) == Ix[indy][indx]:
+        m_align = m[indx] + m_align
+        n_align = "-" + n_align
+    elif box_max(indx,indy) == Iy[indy][indx]:
+        m_align = "-" + m_align
+        n_align = n[indy] + n_align
     while indx !=0 and indy != 0:
-        i = indx
-        j = indy
-        max_dir = max(box_max(i-1,j-1),
-                  box_max(i-1,j),
-                  box_max(i,j-1))
-        if max_dir == box_max(i-1,j-1):
-            m_align = m[i-1] + m_align
-            n_align = n[j-1] + n_align
+        #if box_max(indx,indy) == D
+        max_dir = max(box_max(indx-1,indy-1),
+                      box_max(indx-1,indy),
+                      box_max(indx,indy-1))
+        if max_dir == box_max(indx-1,indy-1):
+            m_align = m[indx-1] + m_align
+            n_align = n[indy-1] + n_align
             indx = indx-1
-            indy = indy-1
-        elif max_dir == box_max(i-1,j):
-            m_align = m[i-1] + m_align
+            indy = indy-1 
+        elif max_dir == box_max(indx-1,indy):
+            m_align = m[indx-1] + m_align
             n_align = "-" + n_align
             indx = indx-1
-        elif max_dir == box_max(i,j-1):
+        elif max_dir == box_max(indx,indy-1):
             m_align = "-" + m_align
-            n_align = n[j-1] + n_align
+            n_align = n[indy-1] + n_align
             indy = indy-1
         
     print (m_align)
     print (n_align)       
-filename = "input5.txt"
+filename = "input2.txt"
 if os.path.exists(filename):
     lines = open(filename, 'r')
     lines = list(lines)
@@ -96,9 +111,23 @@ n = lines[2][:-1] # n-mer sequence of length n : used as the Y-axis
 n_len = len(n)
 inf = float('inf')
 
+anchor_pairs = {}
+if k == 0:
+    g = 0
+elif k >= 1:
+    s = s*k 
+    for anchor in range(k):
+        pair = lines[anchor+3][:-1].split()
+        anchor_pairs[int(pair[0])] = int(pair[1])
+    
+
 D = np.array([[-inf]*m_len]*n_len) #diagonal movement
 Ix = np.array([[-inf]*m_len]*n_len) #horizontal movement
 Iy = np.array([[-inf]*m_len]*n_len) #vertical movement
 
 initialize()
-recurse()             
+
+print (D)
+print (Ix)
+print (Iy)
+align()
